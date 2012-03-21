8ec29baf46000921617fed8a4109fed2487ded4c
lacop <laco.papay@gmail.com>
2012-03-21 17:53:41 +0100

    Added backtrace printing on error. Enabled by a compilation flag, -DDEBUG_BACKTRACE_MAXLENGTH. Added some build info to the notes.txt.

--- util.c
+++ util.c
@@ -2,10 +2,49 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#ifdef DEBUG_PRINT_BACKTRACE
+#include <execinfo.h>
+#endif 
+
+#include "util.h"
+
+
+#ifdef DEBUG_PRINT_BACKTRACE
+#ifndef DEBUG_BACKTRACE_MAXLENGTH
+#define DEBUG_BACKTRACE_MAXLENGTH 20
+#endif
+
+void *_backtrace_buffer[DEBUG_BACKTRACE_MAXLENGTH];
+#endif 
+
+// TODO: Put the attribute to definition, if possible
+inline void _debug_print_backtrace() __attribute__((always_inline));
+inline void _debug_print_backtrace()
+{
+#ifdef DEBUG_PRINT_BACKTRACE
+    size_t size = backtrace(_backtrace_buffer, DEBUG_BACKTRACE_MAXLENGTH);
+    char **symbols = backtrace_symbols(_backtrace_buffer, size);
+    if (symbols == NULL)
+    {
+        fprintf(stderr, "\tBacktrace failed\n");
+        return;
+    }
+    
+    fprintf(stderr, "\tBacktrace:\n");
+    for(int i = 0; i < size; i++)
+    {
+        fprintf(stderr, "\t[%02d] %s\n", i, symbols[i]);
+    }
+    
+    free(symbols);    
+#endif
+}
+
 int _error_print(const char *scope, const char *call, const char *file, int line)
 {
     fprintf(stderr, "Error: [%s] %s\n\tIn file %s at line %d\n\t", scope, call, file, line);
     perror(NULL);
+    _debug_print_backtrace();
     return -1;
 }
 
