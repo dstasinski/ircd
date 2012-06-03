d230048a76f6895030cb42485201bdee6fec6e93
lacop <laco.papay@gmail.com>
2012-06-03 16:19:58 +0200

    Another linked list of clients bugfix.

--- client.c
+++ client.c
@@ -59,7 +59,11 @@ void client_delete(client_data* client)
     {
         clients = client->next;
     }
-    client->next->prev = client->prev;
+    
+    if (client->next != NULL)
+    {
+        client->next->prev = client->prev;
+    }
     
     // Clean up all data stored in this client's client_data
     if (client->send_queue_start != NULL)
